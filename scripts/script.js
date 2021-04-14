638980602db528162e091c97eccd1c493a439df7
Thiago Pereira <thiago.spereira02@gmail.com>
2021-04-14 20:57:54 -0300

    Adiçao dos requisitos de entrada na sala

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,27 @@
-let user;
+let user = "";
 
 function login(){
     user = document.querySelector(".name").value;
-    console.log(user)
-    const promise = axios.post("", user);
+    const data = {
+        name: user
+    };
+    const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", data);
+    console.log(promise)
     promise.then(loadingPage);
     promise.catch(error);
 }
 
 function error(){
-    user = prompt("Nome não disponível. Por favor, digite outro")
-    const promise = axios.post("", user);
+    user = prompt("Nome não disponível. Por favor, digite outro");
+    const data = {
+        name: user
+    };
+    const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", data);
     promise.then(loadingPage);
     promise.catch(error);
 }
 
-function loadingPage(){
+function loadingPage(response){
     const element = document.querySelector(".login") 
     element.classList.add("hidden");
     setTimeout(chatPage, 3000);
@@ -23,8 +29,26 @@ function loadingPage(){
 
 function chatPage(){
     const element = document.querySelector(".loading")
-    element.classList.add("hidden");    
+    element.classList.add("hidden");
+    setInterval(update,5000);    
 }
+
+function update(){
+    const data = {
+        name: user
+    };
+    const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status", data);
+    console.log(promise)
+}
+
+
+
+
+
+
+
+
+
 
 function activeParticipants(){
     const element = document.querySelector("aside");
